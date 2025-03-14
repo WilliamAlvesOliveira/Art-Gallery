7f776ae990873af5c61e7e9cfb0416bed50ca040
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-03-14 16:52:50 -0300

    cssupdate

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import Carousel from './components/carousel';
 
 function App() {
     return (
-        <div className="App">
+      
+        <div className="App"> 
           <NavBar />
-          <Carousel />  
+          <main>
+            <section id="sobre">
+              <p>O projeto ART Gallery é uma iniciativa voltada para o desenvolvimento de uma página web interativa utilizando a biblioteca React. O objetivo principal é criar uma experiência visual envolvente e dinâmica, incorporando diversas técnicas modernas de exibição de imagens.</p>
+            </section>
+            <Carousel /> 
+          </main> 
         </div> 
     );
 } 
