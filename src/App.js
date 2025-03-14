675e8821a9c6282423ec296168cffa54c6af6448
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-03-14 13:58:46 -0300

    addicinando carrossel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-import SectionCarrossel from '../src/components/CarrosselText'
+import Carousel from './components/carousel';
 
 
 function App() {
     return (
         <div className="App">
           <NavBar />
+          <Carousel />  
         </div> 
     );
-}
+} 
 
 export default App;
 
